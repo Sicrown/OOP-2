30c6733a66599ec7154279e7a74880ffaed9bac3
ilya <107561657+Sicrown@users.noreply.github.com>
2022-12-13 18:07:15 +0300

    Main

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
+        System.out.println("\nSpeak");
+        zoo.talk();
+        System.out.println("\nRun");
+        zoo.run();
+        System.out.println("\nFly");
+        zoo.fly();
+        System.out.println("\nSwim");
+        zoo.swim();
+    }
+}
